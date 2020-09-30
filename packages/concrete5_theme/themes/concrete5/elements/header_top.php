25195b426ab9a14da14cf7ab539d3a48c0819481
Korvin Szanto <me@kor.vin>
2020-09-30 14:13:39 -0700

    Add support for google tag manager if an ID is provided through the GTM_ID environment variable

--- packages/concrete5_theme/themes/concrete5/elements/header_top.php
+++ packages/concrete5_theme/themes/concrete5/elements/header_top.php
@@ -2,6 +2,20 @@
 <!DOCTYPE html>
 <html lang="<?=Localization::activeLanguage()?>">
 <head>
+    <?php
+    $gtm = getenv('GTM_ID');
+    if ($gtm) {
+        ?>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','<?= $gtm ?>');</script>
+        <!-- End Google Tag Manager -->
+        <?php
+    }
+    ?>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" type="text/css" href="<?=$view->getThemePath()?>/css/main.css">
     <?php Loader::element('header_required', array('pageTitle' => $pageTitle));?>
@@ -28,5 +42,15 @@
 
 </head>
 <body>
+<?php
+if ($gtm) {
+    ?>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?= $gtm ?>"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <?php
+}
+?>
 
 <div class="<?=$c->getPageWrapperClass()?>">
